6ba558a2a796ea0d96ef9b0b622b5410330c5985
0xWelsh <maguamuhuri@gmail.com>
2025-11-10 08:29:43 +0300

    12/12

--- tutorialspoint/median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
+++ tutorialspoint/median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
@@ -1,37 +1,49 @@
 public class MedianOfTwoSortedArrays {
 	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-	if (nums1.length > nums2.length) {
-		return findMedianSortedArrays(num2, nums1);
-	}
+		if (nums1.length > nums2.length) {
+			return findMedianSortedArrays(nums2, nums1);
+		}	
 
-	int m = nums1.length;
-	int n = nums2.length;
-	int totalLeft = (m + n + 1) / 2;
+		int m = nums1.length;
+		int n = nums2.length;
+		int totalLeft = (m + n + 1) / 2;
 
-	int left = 0, right = m;
+		int left = 0, right = m;
+						
+		while (left <= right) {
+			int partition1 = (left + right) / 2;
+			int partition2 = totalLeft - partition1;
 
-	while (left <= right) {
-		int partition1 = (left + right) / 2;
-		int partition2 = totalLeft - partition1;
+			int maxLeft1 = (partition1 == 0) ? Integer.MIN_VALUE : nums1[partition1 - 1];
+			int minRight = (partition2 == m) ? Integer.MAX_VALUE : nums2[partition1];
 
-		int maxLeft1 = (partition1 == 0) ? Integer.MIN_VALUE : nums1[partition1 - 1];
-		int minRight = (partition2 == m) ? Integer.MAX_VALUE : nums2[partition1];
+			int maxLeft2 = (partition2 == 0) ? Integer.MIN_VALUE : nums2[partition2 - 1];
+			int minRight2 = (partition2 == n) ? Integer.MAX_VALUE : nums2[partition2];
 
-		int maxLeft2 = (partition2 == 0) ? Integer.MIN_VALUE : nums2[partition2 - 1];
-		int minRight2 = (parition2 == n) ? Integer.MAX_VALUE : nums2[partition2];
-
-		if (maxLeft1 <= minRight2 && maxLeft2 <= minRight) {
-			if ((m + n) % 2 == 0) {
-				return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+			if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+				if ((m + n) % 2 == 0) {
+					return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+				} else {
+					return Math.max(maxLeft1, maxLeft2);
+				}
+			} else if (maxLeft1 > minRight2) {
+				right = partition1 - 1;
 			} else {
-				return Math.max(maxLeft1, maxLeft2);
-			}
-		} else if (maxLeft1 > minRight2) {
-			right = partition1 - 1;
-		} else {
-			left = partition1 + 1;
+				left = partition1 + 1;
 
+			}	
 		}
+
+		throw new IllegalArgumentException("Input arrays are not sorted.");
 	}
+
+	public static void main(String[] args) {
+		int[] nums1 = {1, 3};
+		int[] nums2 = {2};
+		System.out.println("Median: " + findMedianSortedArrays(nums1, nums2));
+
+		int[] nums3 = {1, 2};
+		int[] nums4 = {3, 4};
+		System.out.println("Median: " + findMedianSortedArrays(nums3, nums4));
 	}
 }
